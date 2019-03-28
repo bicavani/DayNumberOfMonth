cf9a28e20123d53de67522731a3561b7f8e05537
Namlongcomputer <hoandm91@gmail.com>
2019-03-28 15:16:15 +0700

    v1

---
+++ main.js
@@ -0,0 +1,41 @@
+function dayNumberOfMonth() {
+    let month = document.getElementById('month').value;
+    switch (month) {
+        case '1' :
+            alert('31 day');
+            break;
+        case '2' :
+            alert('28 day');
+            break;
+        case '3' :
+            alert('31 day');
+            break;
+        case '4' :
+            alert('30 day');
+            break;
+        case '5' :
+            alert('31 day');
+            break;
+        case '6' :
+            alert('30 day');
+            break;
+        case '7' :
+            alert('31 day');
+            break;
+        case '8' :
+            alert('31 day');
+            break;
+        case '9' :
+            alert('30 day');
+            break;
+        case '10' :
+            alert('31 day');
+            break;
+        case '11' :
+            alert('30 day');
+            break;
+        case '12' :
+            alert('31 day');
+            break;
+    }
+}
